eb1bfa0c8bff7403b018e569bdc454d0ebdd9206
Perez Jose <hoseluisperez@gmail.com>
2021-11-16 16:15:28 -0300

    Carrito

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { useCartContext } from '../../context/CartContext'
 
 export const Cart = () => {
+    let totalPrize = 0
+    const {cartList} = useCartContext()
+    console.log('cartList: ')
+    console.log(cartList)
     return (
         <div>
             <h1> Cart</h1>
+            {  cartList.map((prod) => <li>Raza: {prod.animal.raza} .Cantidad: {prod.quantity} Precio: ${prod.animal.price}</li>
+                
+            )   }
         </div>
     )
 }
