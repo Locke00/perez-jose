1a68768ed0b0c35af241584bb53586b64f717dd6
Perez Jose <hoseluisperez@gmail.com>
2021-12-09 00:24:52 -0300

    Permitido mostrar navbar en moviles

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom";
 import { CartWidget } from "./CartWidget"
 
+
 export function NavBar() {
   return (
-    <nav class="blue">
+    <nav className='blue'>
       <div className="nav-wrapper">
         <CartWidget />
-        <Link to='/' className="brand-logo">Tienda de Mascotas</Link>       
-        <ul id="nav-mobile" className="right hide-on-med-and-down">
+        <Link to='/' className="brand-logo truncate">Tienda</Link>       
+        <ul className="right blue">
           <li><Link to="/">Inicio</Link></li>
           <li><Link to="/">Explorar Animales</Link></li>
           <li><Link to="/categoria/perro">Perros</Link></li>
@@ -16,7 +17,6 @@ export function NavBar() {
         </ul>
       </div>
     </nav>
-
     
   )
 }
